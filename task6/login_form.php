82ee5aeca1663a7247329cc59bd185f82586b6f1
vbolshak <bvy.vetal@gmail.com>
2015-07-28 13:32:35 +0300

    Create login_form.php

---
+++ task6/login_form.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <form method="post" action="index.php">
+           <table>
+                <tr>
+                    <td>E-mail:</td>
+                    <td><input type="email" name="userid"></td></tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><input type="password" name="password"></td></tr>
+                <tr><td colspan="2" align="center">
+                <input type="submit" value="Log in"></td></tr>
+           </table>
+        </form>
+    </body>
+</html>
